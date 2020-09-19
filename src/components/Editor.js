74c4561fcc7da0b55e8853c1808f4feebc99df9e
Nishant garg <iamnishantgarg@gmail.com>
2020-09-19 22:46:53 +0530

    basic editor theme

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "codemirror/lib/codemirror.css";
-import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
 import "codemirror/mode/xml/xml";
 import "codemirror/mode/javascript/javascript";
 import "codemirror/mode/css/css";
